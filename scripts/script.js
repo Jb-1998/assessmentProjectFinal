eafeb9fd95d77efc9d9b24657629e4470fdd907d
John Benedick <jbstaromana.jbsr@gmail.com>
2020-07-13 23:38:02 +0800

    fixed styles and animations

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,3 @@
-gsap.from(".intro-title", { opacity: 0, duration: 4, y: -50 });
-gsap.from(".phone-style", { opacity: 0, duration: 4, x: 50 });
-
 const text = document.querySelector("#animation");
 const strText = text.textContent;
 const splitText = strText.split("");
@@ -29,15 +26,57 @@ function complete() {
   timer = null;
 }
 
-function scrollAppearing() {
-  let chatbotText = document.querySelector(".chatbot-description");
-  let chatbotTextPos = chatbotText.getBoundingClientRect().top;
-  //console.log(chatbotTextPos);
-  let screenPosition = window.innerHeight;
+ScrollOut({
+  once: true,
+  threshold: 0.9,
 
-  if (chatbotTextPos < screenPosition) {
-    chatbotText.classList.add("chatbot-appear");
+  // onShown: function (el) {
+  //   // use the web animation API
+  //   el.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
+  // },
+});
+
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        700,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
+
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.querySelector("nav");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
   }
 }
 
-window.addEventListener("scroll", scrollAppearing());
+gsap.from(".intro-title", { opacity: 0, duration: 4, y: -50 });
+gsap.from(".phone-style", { opacity: 0, duration: 4, x: 50 });
